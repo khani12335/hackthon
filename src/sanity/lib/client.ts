416f8b93f69233c3d77de780340e94a462dd63f0
dani_khan <159428334+khani12335@users.noreply.github.com>
2025-02-07 20:09:42 +0500

    update-commit

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,11 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+// import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: "gtq2b2p8",
+  dataset: "production",
+  apiVersion:"2024-02-07",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  token:"skCcGEftffU7MjIfAmEfMdy6WfxyeJb4CXovnlcnoKecHX9TuL8vL4lFSeoZfuS345ovR0V2nPo0a30s4kMr9zoYu3DDsDOtTq06wz4nsfTeka8TuMBqT5rdfYDf6t1pi16pxHeKZRAra4EoVoTZqGb3WOWOAJ0bYluguSkCTN6H9JGZsV0R"
 })
